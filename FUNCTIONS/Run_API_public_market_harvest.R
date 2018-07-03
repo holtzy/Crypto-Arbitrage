e74c902171be8b2ad9117d9916b6dbe106716bc4
Holtz Yan <yan.holtz.data@gmail.com>
2018-07-03 11:00:49 +1000

    Add arbitrage functions

---
+++ FUNCTIONS/Run_API_public_market_harvest.R
@@ -0,0 +1,78 @@
+# This script call the API of plateforms during an infinite amount of time.
+# It build a huge 'ticker' data frame
+
+# 3 arguments: 
+  # - currency: BTCEUR / ETHEUR / XRPEUR / LTCEUR / BCHEUR
+  # - Time to wait during 2 API calls
+  # - output file (.R format)
+
+# This script can be run locally or on the cluster doing:
+# -- Cluster
+# 
+# qsub -b y -cwd -e tmp_BTCEUR.e -o tmp_BTCEUR.o -l vf=10G,h_vmem=10G   "/clusterdata/apps/R-3.2.3/bin/Rscript  /ibscratch/wrayvisscher/Yan_Holtz/X_CRYPTO/Run_API_public_market_harvest.R BTCEUR 1 /ibscratch/wrayvisscher/Yan_Holtz/X_CRYPTO/PublicMarket_BTCEUR.R"
+
+# -- Locally
+# cd ~/Dropbox/CRYPTO/4_DATASET
+# Rscript ../3_FUNCTIONS/Run_API_public_market_harvest.R BTCEUR 10 ticked_data_BTC_26_jun.Rdata
+# Rscript ../3_FUNCTIONS/Run_API_public_market_harvest.R ETHEUR 10 ticked_data_ETH_26_jun.Rdata
+# Rscript ../3_FUNCTIONS/Run_API_public_market_harvest.R XRPEUR 10 ticked_data_XRP_26_jun.Rdata
+# Rscript ../3_FUNCTIONS/Run_API_public_market_harvest.R LTCEUR 10 ticked_data_LTC_26_jun.Rdata
+# Rscript ../3_FUNCTIONS/Run_API_public_market_harvest.R BCHEUR 10 ticked_data_BCH_26_jun.Rdata
+
+
+
+# Get the arguments
+args = commandArgs(trailingOnly=TRUE)
+currency=args[1]        # Must be one of BTCEUR or ETHEUR
+my_sleep_time=args[2]
+output=args[3]
+
+# Library requested:
+library(RCurl)          # To request data from an API
+library(RJSONIO)        # To transform a JSON format in a data frame.
+library(dplyr)
+
+# Source the functions to request each API
+source("~/Dropbox/CRYPTO/3_FUNCTIONS/Public_Market_Functions.R")
+
+#Initialize an emtpy result table
+Ticker=as.data.frame(matrix(NA, 0, 12))
+names(Ticker) = c("time", "platform", "ask", "bid", "last", "open", "low", "high", "volume", "volumeQuote", "timestamp", "symbol")
+for(i in c(3:12)){Ticker[,i]=as.numeric(as.character(Ticker$ask))}
+
+# Run the analysis
+num=0
+while(TRUE){
+  num=num+1
+  cat("---------")
+  cat(num)
+  time=Sys.time()
+  
+  if(currency %in% c("BTCEUR", "ETHEUR", "LTCEUR")){
+    cat("harvest coinbase")
+    tmp=try(get_coinbase(time, currency))
+    if(is.data.frame(tmp)){ Ticker=rbind(Ticker, tmp) }
+  }
+  
+  cat(" / kraken")
+  tmp=try(get_kraken(time, currency))
+  if(is.data.frame(tmp)){ Ticker=rbind(Ticker, tmp) }
+  
+  cat(" / bitstamp")
+  tmp=try(get_bitstamp(time, currency))
+  if(is.data.frame(tmp)){ Ticker=rbind(Ticker, tmp) }
+  
+  if(currency %in% c("BTCEUR", "ETHEUR", "BCHEUR")){
+    print(" / cex.io")
+    tmp=try(get_cex(time, currency))
+    if(is.data.frame(tmp)){ Ticker=rbind(Ticker, tmp) }
+  }
+  
+  # Every ten loop I save the file
+  if (num %% 10 == 0){
+      cat("--------- SAVE -------- ")
+      save(Ticker, file=output)
+  }
+  Sys.sleep(my_sleep_time)
+  
+}
---
+++ FUNCTIONS/Run_Arbitrage_Bitstamp_Cex.R
@@ -0,0 +1,236 @@
+# This script allows to make arbitrage between Bitstamp and Cex. Each time unit it does:
+#     - get Bitstamp and Cex BCH Ticker
+#     - calculate the difference in %
+#     - If i/ the difference is big enough ii/ it is interesting to make a transfert to balance accounts --> make the transaction
+#     - produce a report file allowing to study the behaviour of the bot
+
+# arguments: 
+# - Currency, can be BTCEUR / ETHEUR / XRPEUR / LTCEUR / BCHEUR
+# - quantity to trade (in euro). must be at least 21 to respect CEX threshold.
+# - threshold 
+# - threshold that I accept for rebalancing data
+# - output file (.R format)
+
+
+# This script can be run locally doing:
+# Rscript Run_Arbitrage_Bitstamp_Cex.R "BCHEUR" 21 0.9 0.5  arbitrage_bitstamp_cex_BCHEUR_7Jun.Rdata
+
+
+
+# 0 ------ INITIALISATION
+
+# Get the arguments
+args = commandArgs(trailingOnly=TRUE)
+currency=args[1]
+euro_to_trade=as.numeric(args[2])
+thres=as.numeric(args[3])
+thres_rebalance=as.numeric(args[4])
+output=args[5]
+
+# Library requested:
+library(RJSONIO)        # To transform a JSON format in a data frame.
+library(dplyr)
+library(digest)         # For the hmac function that allows to create the signature
+library(RCurl)          # Pour récupérer les données en appelant des URLS
+require(jsonlite)       # To go from JSON to data frame
+
+# And source the functions I've written
+# public API
+source("https://raw.githubusercontent.com/holtzy/Cryp-To/master/FUNCTIONS/Public_Market_Functions.R")
+# private API for Bitstamp
+source("https://raw.githubusercontent.com/holtzy/Cryp-To/master/FUNCTIONS/Private_API_Functions_Bitstamp.R")
+# private API for Cex
+source("https://raw.githubusercontent.com/holtzy/Cryp-To/master/FUNCTIONS/Private_API_Functions_Cex.R")
+
+
+
+# 0 ------ CODES
+# This script needs to access you code to the Bitstamp and Cex private APIs.
+# You need to create 6 objects for the script to work: key_cex / secret_cex / customer_id_cex / key_bitstamp / secret_bitstamp / customer_id_bitstamp
+# For my personal usage, I source this object on a file that is not public:
+source("access.R")
+
+
+
+# 1 ------ GET INITIAL BALANCE & INITIALIZE OUTPUTS
+
+# I need to know the balance in each platform to know if I can trade or not.
+
+cat("--- Start initialization \n")
+
+# Get platform1 balance:
+initial_balance_bitstamp=get_my_balance_bitstamp()
+init_crypto_plat1 = as.numeric(as.character(initial_balance_bitstamp[ 1, "bch_available"] ))
+init_euro_plat1 = as.numeric( as.character( initial_balance_bitstamp[ 1, "eur_available"] ))
+
+# Get platform2 balance:
+initial_balance_cex=get_my_balance_cex()
+init_crypto_plat2 = as.numeric( as.character( initial_balance_cex[ 1, "BCH.available"] ))
+init_euro_plat2 = as.numeric( as.character( initial_balance_cex[ 1, "EUR.available"] ))
+
+# Initialize outputs. I will have one line per iteration of the loop. If I do a transaction , some column will be filled with the appropriate information.
+bilan=as.data.frame(matrix(NA, 0, 25))
+names(bilan) = c(
+  "time", "last_plat1", "ask_plat1", "bid_plat1", "last_plat2", "ask_plat2", "bid_plat2", "diff_side1", "diff_side2", "transaction", "rebalance", "real_bitstamp",
+  "thres", "thres_rebalance", "euro_to_trade", "crypto_to_trade",
+  "euro_plat1", "crypto_plat1", "euro_plat2", "crypto_plat2",
+  "total_euro", "total_crypto", "total", "total_without_arbitrage", "id"
+  )
+
+cat("--- Initialization step OK \n\n")
+
+
+
+
+
+
+
+
+# 2 ------ START THE LOOP
+
+cat("--- Start loop \n")
+num=0
+
+while(TRUE){
+  num=num+1
+  time=Sys.time()
+  real_bitstamp=NA
+  cat(paste("\n\n--------- Call # ", num, " | Time: ", time, " ----------\n"))
+  
+  # ---- Step0: initialize my current amount of money for the start of the loop
+  if( exists("current_crypto_plat1")==FALSE ){
+    current_crypto_plat1=init_crypto_plat1
+    current_crypto_plat2=init_crypto_plat2
+    current_euro_plat1=init_euro_plat1
+    current_euro_plat2=init_euro_plat2
+    cat("Step0 : ok\n")
+  }
+  
+  # ---- Step1: recover price of both platforms
+  tmp=try(get_bitstamp(time, currency))
+  if(is.data.frame(tmp)){ 
+    ask_plat1 = as.numeric( as.character( tmp$ask))  
+    bid_plat1 = as.numeric( as.character( tmp$bid))
+    price_plat1 = as.numeric( as.character( tmp$last))
+  }
+  tmp=try(get_cex(time, currency))
+  if(is.data.frame(tmp)){ 
+    ask_plat2 = as.numeric( as.character( tmp$ask))  
+    bid_plat2 = as.numeric( as.character( tmp$bid))
+    price_plat2 = as.numeric( as.character( tmp$last))
+  }
+  cat( paste("    prices: ", ask_plat1, "/", bid_plat1, " - ", ask_plat2, "/", bid_plat2, "\n", sep=""))
+  cat("Step1: ok\n")
+  
+  # ---- Step2: calculate difference between both plateform? Side1 = platform1 is more expensive. So I buy on plat2 and sell on plat1
+  diff_side1 = (bid_plat1 - ask_plat2) / mean( c(bid_plat1,ask_plat2) ) * 100
+  diff_side2 = (bid_plat2 - ask_plat1) / mean( c(bid_plat2,ask_plat1) ) * 100
+  cat( paste( "    relative diff side1 / side2: ", round(diff_side1,3), " / ", round(diff_side2,3), "\n", sep=""))
+  cat("Step2: ok\n")
+  
+  
+  # ---- Step3: calcule the equivalence crypto / euro --> we need that to trade the good amount
+  crypto_to_trade=( euro_to_trade /  min(bid_plat1, bid_plat2) )
+  cat("Step3: ok\n")
+  
+  
+  # ---- Step4: if there is a significant difference + I have the money, I make a transaction + I check my balance. 
+  trade_side1 = diff_side1 > thres & current_crypto_plat1>crypto_to_trade & current_euro_plat2>euro_to_trade
+  trade_side2 = diff_side2 > thres & current_crypto_plat2>crypto_to_trade & current_euro_plat1>euro_to_trade
+  # I can also make a transaction to rebalance my fundings! If I have less than one third of the crypto in a plateform, I re-balance
+  tot_crypto = current_crypto_plat1 + current_crypto_plat2
+  rebalance_side1 = current_crypto_plat2<tot_crypto/3  & diff_side1 > thres_rebalance & current_crypto_plat1>crypto_to_trade & current_euro_plat2>euro_to_trade
+  rebalance_side2 = current_crypto_plat1<tot_crypto/3  & diff_side2 > thres_rebalance & current_crypto_plat2>crypto_to_trade & current_euro_plat1>euro_to_trade
+  
+  if( trade_side1==TRUE | trade_side2==TRUE | rebalance_side1==TRUE | rebalance_side2==TRUE ){
+    
+    cat("I make a transaction\n")
+    transaction="yes"
+    if( rebalance_side1==TRUE | rebalance_side2==TRUE ){ rebalance="yes" }
+
+    # -- 4.1 if side1 --> plateform1 > plateform2 --> I buy crypto on plateform2, and I sell crypto on plateform1
+    if( trade_side1==TRUE | rebalance_side1==TRUE){
+      cat("    option 1 trade\n")
+      tmp=try( from_crypto_to_euro_bitstamp( crypto_to_trade, currency) )
+      if(is.data.frame(tmp) & ncol(tmp)==5){ cat("    bitstamp transaction ok\n") ; real_bitstamp=as.numeric(as.character(tmp$price)) }else{ cat("    bitstamp transaction FAILED\n")}
+      tmp=try( from_euro_to_crypto_cex( euro_to_trade*1.0025, currency) )
+      if(is.data.frame(tmp)){ cat("    cex transaction ok\n") }else{ cat("    cex transaction FAILED\n")}
+    }
+    
+    # -- 4.2 if side 2 --> plateform1 < plateform2 --> I buy crypto on plateform1, and I sell crypto on plateform2
+    if( trade_side2==TRUE | rebalance_side2==TRUE){
+      cat("    option 2 trade\n")
+      tmp=try( from_euro_to_crypto_bitstamp( crypto_to_trade, currency) )
+      if(is.data.frame(tmp) & ncol(tmp)==5){ cat("    bitstamp transaction ok\n") ; real_bitstamp=as.numeric(as.character(tmp$price)) }else{ cat("    bitstamp transaction FAILED\n")}
+      tmp=try( from_crypto_to_euro_cex( crypto_to_trade, currency) )
+      if(is.data.frame(tmp)){ cat("    cex transaction ok\n") }else{ cat("    cex transaction FAILED\n")}
+    }
+
+    # -- 4.3 And in any case I check how much money I have now
+    tmp=try(get_my_balance_bitstamp())
+    if(is.data.frame(tmp)){
+      current_crypto_plat1 = as.numeric( as.character( tmp[1, "bch_available"] ))
+      current_euro_plat1 = as.numeric( as.character( tmp[1, "eur_available"] ))
+      cat("    Bitstamp balance recovered\n")
+    }else{ cat("    Bitstamp balance NOT recovered\n") }
+    tmp=try(get_my_balance_cex())
+    if( class(tmp)!="try-error" ){
+      current_crypto_plat2 = as.numeric( as.character( tmp$BCH.available ))
+      current_euro_plat2 = as.numeric( as.character( tmp$EUR.available ))
+      cat("    Cex balance recovered\n")
+    }else{ cat("    Cex balance NOT recovered\n") }
+    
+    
+        
+  # ---- Step 4: If no significative difference, then I don't do anything, and NA to the bilan table  
+  }else{
+    cat("I don't do anything\n")
+    transaction="no"
+    rebalance="no"
+  }
+
+  # ---- Step5: make a summary data frame for this time unit and add it to the final output data frame
+  total_euro=current_euro_plat1 + current_euro_plat2
+  total_crypto=current_crypto_plat1 + current_crypto_plat2
+  total= total_euro + current_crypto_plat1*price_plat1 + current_crypto_plat2*price_plat2
+  total_without_arbitrage= init_euro_plat1 + init_euro_plat2 + init_crypto_plat1*price_plat1 + init_crypto_plat2*price_plat2
+  bilan[num,]=c( time, price_plat1, ask_plat1, bid_plat1, price_plat2, ask_plat2, bid_plat2, diff_side1, diff_side2, transaction, rebalance, real_bitstamp, thres, thres_rebalance, euro_to_trade, crypto_to_trade, current_euro_plat1, current_crypto_plat1, current_euro_plat2, current_crypto_plat2, total_euro, total_crypto, total, total_without_arbitrage, num)
+
+  # ---- Step6: Once in a while, I save the summary data frame
+  if (num %% 2 == 0){
+    cat("Save the bilan file")
+    save(bilan, file=output)
+  }
+  
+  # ---- I wait a little bit between each occurence.
+  Sys.sleep(5)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
